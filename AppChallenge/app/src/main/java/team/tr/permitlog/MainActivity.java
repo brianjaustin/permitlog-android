3084366d2bfb8ccd901487026cbb701c1f1856ac
Brian Austin <brianjaustin@gmail.com>
2017-01-21 17:44:10 -0500

    Setup Firebase UI login screen with Google authentication.

--- AppChallenge/app/src/main/java/team/tr/permitlog/MainActivity.java
+++ AppChallenge/app/src/main/java/team/tr/permitlog/MainActivity.java
@@ -3,11 +3,38 @@ package team.tr.permitlog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseUser currentUser;
+
+    // Sign in request code
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get the current user from Firebase. If no user is logged in,
+        // show the FirebaseUI login screen.
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (currentUser == null) {
+            startActivityForResult(
+                    AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setProviders(Arrays.asList(
+                                new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()
+                        ))
+                        .build(),
+                    RC_SIGN_IN
+            );
+        }
     }
 }
