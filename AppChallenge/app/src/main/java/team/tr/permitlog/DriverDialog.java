9cd6ade079156ebab2a15e6fb71b755aea06c2fe
Brian Austin <brianjaustin@gmail.com>
2017-01-27 16:42:06 -0500

    Added code to set the title of the driver add/edit dialog.

--- AppChallenge/app/src/main/java/team/tr/permitlog/DriverDialog.java
+++ AppChallenge/app/src/main/java/team/tr/permitlog/DriverDialog.java
@@ -1,13 +1,35 @@
 package team.tr.permitlog;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class DriverDialog extends AppCompatActivity {
+
+    private boolean editing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_dialog);
+
+        Intent intent = getIntent();
+        if (!intent.getStringExtra("driverId").toString().equals("")) {
+            // A driver is being edited
+            editing = true;
+
+            // Set title
+            getSupportActionBar().setTitle(R.string.driver_edit_title);
+
+            // TODO: Add code to retreive values
+        }
+        else {
+            // A new driver is being added
+            editing = false;
+
+            // Set title
+            getSupportActionBar().setTitle(R.string.driver_add_title);
+        }
     }
 }
