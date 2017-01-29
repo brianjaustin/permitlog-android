1bfebf7c08496ef325bbc9095d71f63aaa8e4dd3
Brian Austin <brianjaustin@gmail.com>
2017-01-29 14:26:04 -0500

    Added skeleton add/update code to the DriverDialog.

--- AppChallenge/app/src/main/java/team/tr/permitlog/DriverDialog.java
+++ AppChallenge/app/src/main/java/team/tr/permitlog/DriverDialog.java
@@ -6,17 +6,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class DriverDialog extends AppCompatActivity {
 
+    // Whether or not this dialog is being used to edit an existing driver
     private boolean editing;
+
+    // Firebase reference
+    private DatabaseReference driverRef;
+
+    private String driverId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_dialog);
         Intent intent = getIntent();
+
+        // Setup the firebase reference
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        driverRef = FirebaseDatabase.getInstance().getReference().child(userId).child("drivers");
+
+        // Set the toolbar as the action bar
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
@@ -26,11 +43,12 @@ public class DriverDialog extends AppCompatActivity {
         if (!intent.getStringExtra("driverId").toString().equals("")) {
             // A driver is being edited
             editing = true;
+            driverId = intent.getStringExtra("driverId").toString();
 
             // Set title
             ab.setTitle(R.string.driver_edit_title);
 
-            // TODO: Add code to retreive values
+            // TODO: Add code to retrieve values
         }
         else {
             // A new driver is being added
@@ -40,6 +58,7 @@ public class DriverDialog extends AppCompatActivity {
             ab.setTitle(R.string.driver_add_title);
         }
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home ) {
@@ -50,6 +69,30 @@ public class DriverDialog extends AppCompatActivity {
         // other menu select events may be present here
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onSaveClick(View view) { // TODO: actually call this function
+        // Get values from the text fields
+        String firstName = ""; // TODO: Change this to get actual values
+        String lastName = "";
+        String licenseNumber = "";
+
+        if (editing) {
+            // Update existing values
+            DatabaseReference editRef = driverRef.child(driverId);
+            editRef.child("name").child("first").setValue(firstName);
+            editRef.child("name").child("last").setValue(lastName);
+            editRef.child("license_number").setValue(licenseNumber);
+        } else {
+            // Push a new driver
+            DatabaseReference newRef = driverRef.push();
+            newRef.child("name").child("first").setValue(firstName);
+            newRef.child("name").child("last").setValue(lastName);
+            newRef.child("license_number").setValue(licenseNumber);
+        }
+
+        // Close the dialog
+        finish();
     }
 }
 
