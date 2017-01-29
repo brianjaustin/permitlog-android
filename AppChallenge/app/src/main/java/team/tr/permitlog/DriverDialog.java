4a5e68d8db1aad105fb967a4e26a359588bf568a
Nathan Kiesman <nkizz@llama.local>
2017-01-28 23:27:05 -0500

    Improved Drivers Dialog, havnt added text input yet

--- AppChallenge/app/src/main/java/team/tr/permitlog/DriverDialog.java
+++ AppChallenge/app/src/main/java/team/tr/permitlog/DriverDialog.java
@@ -1,9 +1,12 @@
 package team.tr.permitlog;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class DriverDialog extends AppCompatActivity {
 
@@ -13,14 +16,19 @@ public class DriverDialog extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_dialog);
-
         Intent intent = getIntent();
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        ActionBar ab = getSupportActionBar(); //Create action bar object
+        ab.setDisplayHomeAsUpEnabled(true); //Enable back button
+
         if (!intent.getStringExtra("driverId").toString().equals("")) {
             // A driver is being edited
             editing = true;
 
             // Set title
-            getSupportActionBar().setTitle(R.string.driver_edit_title);
+            ab.setTitle(R.string.driver_edit_title);
 
             // TODO: Add code to retreive values
         }
@@ -29,7 +37,19 @@ public class DriverDialog extends AppCompatActivity {
             editing = false;
 
             // Set title
-            getSupportActionBar().setTitle(R.string.driver_add_title);
+            ab.setTitle(R.string.driver_add_title);
         }
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home ) {
+            Toast.makeText(getApplicationContext(), "Overroad", Toast.LENGTH_SHORT).show();
+            finish();
+            return true;
+        }
+        // other menu select events may be present here
+
+        return super.onOptionsItemSelected(item);
+    }
 }
+
