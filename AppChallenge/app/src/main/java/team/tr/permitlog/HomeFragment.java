d28524ed8c53372d748563c0b1657cd6934bb8b1
Nathan Kiesman <nkizz@llama.local>
2017-01-28 23:27:05 -0500

    Improved Drivers Dialog, havnt added text input yet

--- AppChallenge/app/src/main/java/team/tr/permitlog/HomeFragment.java
+++ AppChallenge/app/src/main/java/team/tr/permitlog/HomeFragment.java
@@ -4,11 +4,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+
 import com.github.clans.fab.FloatingActionButton;
 import com.github.clans.fab.FloatingActionMenu;
 
 import android.support.v4.app.Fragment;
-import android.util.Log;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
