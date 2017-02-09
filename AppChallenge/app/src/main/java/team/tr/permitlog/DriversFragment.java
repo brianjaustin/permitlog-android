384559dd5da05624361348252225c7b1b43c0f25
Noble-Mushtak <Noble-Mushtak@users.noreply.github.com>
2017-02-09 18:38:42 -0500

    Fixed bug with DriverAdapter and refactored code

--- AppChallenge/app/src/main/java/team/tr/permitlog/DriversFragment.java
+++ AppChallenge/app/src/main/java/team/tr/permitlog/DriversFragment.java
@@ -4,22 +4,12 @@ package team.tr.permitlog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.ArrayList;
 
 
 public class DriversFragment extends ListFragment {
@@ -38,8 +28,6 @@ public class DriversFragment extends ListFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        LayoutInflater lf = getActivity().getLayoutInflater();
-
         // Get the uid from the main activity
         userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
