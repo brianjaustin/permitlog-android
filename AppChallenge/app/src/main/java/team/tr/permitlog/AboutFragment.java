251603513379c957a480a0b0297a3bf593824f29
Noble-Mushtak <Noble-Mushtak@users.noreply.github.com>
2017-02-13 19:32:06 -0500

    Cleaned up code

--- AppChallenge/app/src/main/java/team/tr/permitlog/AboutFragment.java
+++ AppChallenge/app/src/main/java/team/tr/permitlog/AboutFragment.java
@@ -1,44 +1,26 @@
 package team.tr.permitlog;
 
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.InputMethodManager;
 import android.webkit.WebView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class AboutFragment extends Fragment {
     //For logging:
     public static String TAG = "AboutFragment";
     //The root view for this fragment, used to find elements by id:
     private View rootView;
 
-    public AboutFragment() {
-        // Required empty public constructor
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //Get the WebView element:
         LayoutInflater lf = getActivity().getLayoutInflater();
         rootView =  lf.inflate(R.layout.fragment_about, container, false);
         WebView webView = (WebView) rootView.findViewById(R.id.webView);
+        //Load the HTML file in the WebView:
         webView.loadUrl("file:///android_asset/about.html");
         return rootView;
     }
