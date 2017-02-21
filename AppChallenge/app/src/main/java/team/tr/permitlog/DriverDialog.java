73a2a4b49423d92673f4052618b6eb35db525095
Brian Austin <brianjaustin@gmail.com>
2017-02-21 12:47:54 -0500

    Validate driver's age

--- AppChallenge/app/src/main/java/team/tr/permitlog/DriverDialog.java
+++ AppChallenge/app/src/main/java/team/tr/permitlog/DriverDialog.java
@@ -120,6 +120,12 @@ public class DriverDialog extends AppCompatActivity {
             return;
         }
 
+        // Check to make sure the driver has a valid age
+        if (Integer.parseInt(driverAge) < 21) {
+            Toast.makeText(this, R.string.driver_age_error, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         if (editing) {
             // Update existing values
             DatabaseReference editRef = driverRef.child(driverId);
