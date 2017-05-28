03321368e0ede06d6b62d3883f7a56021ad8bb0f
Nathan Kiesman <n.kizz2711@gmail.com>
2017-05-28 10:12:13 -0400

    Re-added driver sanity check

--- app/src/main/java/team/tr/permitlog/DriverDialog.java
+++ app/src/main/java/team/tr/permitlog/DriverDialog.java
@@ -120,11 +120,11 @@ public class DriverDialog extends AppCompatActivity {
             return;
         }
 
-        // Check to make sure the driver has a valid age
-        //if (Integer.parseInt(driverAge) < 21) {
-        //    Toast.makeText(this, R.string.driver_age_error, Toast.LENGTH_SHORT).show();
-        //    return;
-        //}
+        //Check to make sure the driver has a valid age
+        if (Integer.parseInt(driverAge) < 15 || Integer.parseInt(driverAge) > 117) {
+            Toast.makeText(this, R.string.driver_age_error, Toast.LENGTH_SHORT).show();
+            return;
+        }
 
         if (editing) {
             // Update existing values
