d91297c0ae864f30b30545148d422f4543334709
Brian Austin <brianjaustin@gmail.com>
2018-03-05 07:12:11 -0500

    javadoc for DrivingTimesConsumer

--- app/src/main/java/team/tr/permitlog/DrivingTimesConsumer.java
+++ app/src/main/java/team/tr/permitlog/DrivingTimesConsumer.java
@@ -1,6 +1,15 @@
 package team.tr.permitlog;
 
+/**
+ * An interface for objects that consume a DrivingTimes object and return nothing.
+ *
+ * @see DrivingTimes
+ */
 public interface DrivingTimesConsumer {
-    //A function that in a DrivingTimes object and returns nothing:
+    /**
+     * A function that takes in a DrivingTimes object and returns nothing.
+     *
+     * @param timesObj the DrivingTimes object to be consumed.
+     */
     void accept(DrivingTimes timesObj);
 }
