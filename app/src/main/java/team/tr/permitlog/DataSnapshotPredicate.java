cac7ce85b78ace43f4a783212151ed4f1597da98
Nathan Kiesman <n.kizz2711@gmail.com>
2018-05-24 22:14:26 -0400

    Added privacy policy page

--- app/src/main/java/team/tr/permitlog/DataSnapshotPredicate.java
+++
@@ -1,8 +0,0 @@
-package team.tr.permitlog;
-
-import com.google.firebase.database.DataSnapshot;
-
-public interface DataSnapshotPredicate {
-    //Takes in a DataSnapshot and returns a boolean:
-    boolean accept(DataSnapshot dataSnapshot);
-}
